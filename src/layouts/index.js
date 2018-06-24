a48f0a5fbd4b5f67911af1b9892dc4223bff4379
Kamil Jozwik <kamiljozwikpl@gmail.com>
2018-06-24 22:49:26 +0200

    working intro page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Navbar from '../components/Navbar'
-import './all.sass'
+import '../styles/styles.scss'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet title="Home | Gatsby + Netlify CMS" />
-    <Navbar />
     <div>{children()}</div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import Logo from '../img/Logo.png';
 
 export default class IndexPage extends React.Component {
   render() {
-    const { data } = this.props
-    const { edges: posts } = data.allMarkdownRemark
 
     return (
-      <section className="section">
-        <div className="container">
-          <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">Latest Stories</h1>
+      <section className="landing">
+          <div className="landing-left">
+            <div className="landing__left-panel panel">
+              <img alt="logo" src={Logo} className="landing__left-panel--logo" />
+              <div className="landing__left-panel--enter"><Link to="/main">ENTER</Link></div>
+            </div>
           </div>
-          {posts
-            .map(({ node: post }) => (
-              <div
-                className="content"
-                style={{ border: '1px solid #eaecee', padding: '2em 4em' }}
-                key={post.id}
-              >
-                <p>
-                  <Link className="has-text-primary" to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                  <span> &bull; </span>
-                  <small>{post.frontmatter.date}</small>
-                </p>
-                <p>
-                  {post.excerpt}
-                  <br />
-                  <br />
-                  <Link className="button is-small" to={post.fields.slug}>
-                    Keep Reading →
-                  </Link>
-                </p>
+          <div className="landing-right">
+            <div className="landing__right-panel panel">
+              <div className="landing__right-panel--logo">
+                <span>ZdrowyBus BLOG</span>
               </div>
-            ))}
-        </div>
+              <div className="landing__right-panel--enter">coming soon...</div>
+            </div>
+          </div>
       </section>
     )
   }
@@ -51,26 +35,3 @@ IndexPage.propTypes = {
   }),
 }
 
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      sort: { order: DESC, fields: [frontmatter___date] },
-      filter: { frontmatter: { templateKey: { eq: "blog-post" } }}
-    ) {
-      edges {
-        node {
-          excerpt(pruneLength: 400)
-          id
-          fields {
-            slug
-          }
-          frontmatter {
-            title
-            templateKey
-            date(formatString: "MMMM DD, YYYY")
-          }
-        }
-      }
-    }
-  }
-`
