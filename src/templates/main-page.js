d631ea59399ab14febe3915484fd6a28a0b74620
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-06-26 13:54:53 +0200

    dalszy ESLint

--- src/templates/main-page.js
+++ src/templates/main-page.js
@@ -8,47 +8,44 @@ export const MainPageTemplate = ({
     intro,
     notes,
     ownerBox,
-}) => {
-
-    return (
-        <section className="main">
-            <div className="main__jumbo jumbo">
-                <div className="jumbo__title">{jumbotrone.title}</div>
-                <div className="jumbo__desc">{jumbotrone.description}</div>
-                <div className="jumbo__links links">
-                    <a className="links__yt" href="http://www.youtube.pl"><img alt='YT logo' src={yt}/></a>
-                    <a className="links__fb" href="http://www.facebook.pl"><img alt='FB logo' src={fb}/></a>
+}) => (
+    <section className="main">
+        <div className="main__jumbo jumbo">
+            <div className="jumbo__title">{jumbotrone.title}</div>
+            <div className="jumbo__desc">{jumbotrone.description}</div>
+            <div className="jumbo__links links">
+                <a className="links__yt" href="http://www.youtube.pl"><img alt="YT logo" src={yt} /></a>
+                <a className="links__fb" href="http://www.facebook.pl"><img alt="FB logo" src={fb} /></a>
+            </div>
+        </div>
+        <div className="main_body">
+            <section className="intro">
+                <div className="intro__title title">{intro.title}</div>
+                <div className="intro__desc desc">{intro.description}</div>
+            </section>
+            <section className="notes">
+                <div className="notes__title title">{notes.title}</div>
+                <div className="notes__desc desc">{notes.description}</div>
+            </section>
+            <section className="owner-box">
+                <div className="owner-box__img">
+                    <img
+                        src={ownerBox.avatar.image}
+                        alt={ownerBox.avatar.image}
+                    />
                 </div>
-            </div>
-            <div className="main_body">
-                <section className="intro">
-                    <div className="intro__title title">{intro.title}</div>
-                    <div className="intro__desc desc">{intro.description}</div>
-                </section>
-                <section className="notes">
-                    <div className="notes__title title">{notes.title}</div>
-                    <div className="notes__desc desc">{notes.description}</div>
-                </section>
-                <section className="owner-box">
-                    <div className="owner-box__img">
-                        <img
-                            src={ownerBox.avatar.image}
-                            alt={ownerBox.avatar.image}
-                        />
-                    </div>
-                    <div className="owner-box__title title">{ownerBox.title}</div>
-                    <div className="owner-box__desc desc">{ownerBox.description}</div>
-                </section>
-                <section className="youtube-video">
-                    <div>YOUTUBE</div>
-                </section>
-            </div>
-        </section>
-    );
-}
+                <div className="owner-box__title title">{ownerBox.title}</div>
+                <div className="owner-box__desc desc">{ownerBox.description}</div>
+            </section>
+            <section className="youtube-video">
+                <div>YOUTUBE</div>
+            </section>
+        </div>
+    </section>
+);
 
 const MainPage = ({ data }) => {
-    const {markdownRemark: post} = data;
+    const { markdownRemark: post } = data;
 
     return (
         <MainPageTemplate
@@ -57,12 +54,24 @@ const MainPage = ({ data }) => {
             intro={post.frontmatter.intro}
             notes={post.frontmatter.notes}
             ownerBox={post.frontmatter.ownerBox}
-        />    
-    )
-}
+        />
+    );
+};
 
-export default MainPage
+export default MainPage;
 
+MainPageTemplate.propTypes = {
+    jumbotrone: PropTypes.object,
+    intro: PropTypes.object,
+    notes: PropTypes.object,
+    ownerBox: PropTypes.object,
+};
+
+MainPage.propTypes = {
+    data: PropTypes.object,
+};
+
+/* eslint-disable */
 export const mainPageQuery = graphql`
   query MainPage($id: String!) {
     markdownRemark(id: { eq: $id }) {
@@ -91,4 +100,4 @@ export const mainPageQuery = graphql`
       }
     }
   }
-`
+`;
