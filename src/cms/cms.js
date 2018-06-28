fde6782944f0eda2810b5a1b18abc2e8660af9e3
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-06-28 14:43:58 +0200

    Dodany Team do CMS2

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,6 +5,7 @@ import BlogPostPreview from './preview-templates/BlogPostPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 import MainPagePreview from './preview-templates/MainPagePreview';
 import ProjectPagePreview from './preview-templates/ProjectPagePreview';
+import TeamPagePreview from './preview-templates/TeamPagePreview';
 
 CMS.registerPreviewStyle('/styles.css');
 CMS.registerPreviewTemplate('about', AboutPagePreview);
@@ -12,3 +13,4 @@ CMS.registerPreviewTemplate('main', MainPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
 CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('project', ProjectPagePreview);
+CMS.registerPreviewTemplate('team', TeamPagePreview);
