2c1b1e3dc948abf22003b84081603607cc7a3208
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-06-28 15:21:03 +0200

    Clean CMS from other pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,20 +1,20 @@
 import CMS from 'netlify-cms';
 
-import AboutPagePreview from './preview-templates/AboutPagePreview';
-import BlogPostPreview from './preview-templates/BlogPostPreview';
-import ProductPagePreview from './preview-templates/ProductPagePreview';
+// import AboutPagePreview from './preview-templates/AboutPagePreview';
+// import BlogPostPreview from './preview-templates/BlogPostPreview';
+// import ProductPagePreview from './preview-templates/ProductPagePreview';
 import MainPagePreview from './preview-templates/MainPagePreview';
 import ProjectPagePreview from './preview-templates/ProjectPagePreview';
-import TeamPagePreview from './preview-templates/TeamPagePreview';
 import JoinPagePreview from './preview-templates/JoinPagePreview';
+import TeamPagePreview from './preview-templates/TeamPagePreview';
 import ContactPagePreview from './preview-templates/ContactPagePreview';
 
 CMS.registerPreviewStyle('/styles.css');
-CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('main', MainPagePreview);
-CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('blog', BlogPostPreview);
 CMS.registerPreviewTemplate('project', ProjectPagePreview);
-CMS.registerPreviewTemplate('team', TeamPagePreview);
 CMS.registerPreviewTemplate('team', JoinPagePreview);
+CMS.registerPreviewTemplate('team', TeamPagePreview);
 CMS.registerPreviewTemplate('team', ContactPagePreview);
+// CMS.registerPreviewTemplate('blog', BlogPostPreview);
+// CMS.registerPreviewTemplate('products', ProductPagePreview);
+// CMS.registerPreviewTemplate('about', AboutPagePreview);
