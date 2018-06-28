6b91e3445f9da680a2f6f9edee1ad5344913e773
Kamil Jozwik <kamiljozwikpl@gmail.com>
2018-06-28 22:47:52 +0200

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,24 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
     {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/img/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    },
+    {
       resolve: 'gatsby-plugin-eslint',
       options: {
         test: /\.js$|\.jsx$/,
