1f93e0b746b3bcfbfd5f68618d2cb2b9be0a350a
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-10-17 15:46:04 +0200

    show hide button

--- src/templates/main-page.js
+++ src/templates/main-page.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { TweenMax } from 'gsap/TweenMax';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
 
-export const MainPageTemplate = ({
-  jumbotrone,
-  html,
-  description,
-  path
-}) => (
-  <Layout path={path}>
-    <section className="main component-wrapper">
-      <div className="main__jumbo jumbo">
-        <div className="jumbo__title">{jumbotrone.title}</div>
-        <div className="jumbo__desc">{jumbotrone.description}</div>
-      </div>
-      <div className="main__body component_body">
-        <section className="main-desc">
-          <div className="main-desc__general">{description}</div>
-          <div className="main-desc__purposes cms_content" dangerouslySetInnerHTML={{ __html: html }} />
+class MainPageTemplate extends Component {
+  constructor(props) {
+    super(props);
+    this.jumbotrone = props.jumbotrone;
+    this.html = props.html;
+    this.description = props.description;
+    this.path = props.path;
+    this.purposes = React.createRef();
+  }
+
+  togglePurposes = (event) => {
+    this.purposes.current.classList.toggle('show');
+    // TweenMax.to(this.purposes.current, 0.2, { height: 0 });
+
+    // if(!this.purposes.current.hasClass('show')) {
+    //   TweenMax.to(this.purposes.current, 0.2, { height: 0 });
+    //   $this.addClass("closed")
+    // }else{
+    //   TweenLite.set($content, {height:"auto"})
+    //   TweenLite.from($content, 0.2, {height:0})
+    //   $this.removeClass("closed");
+    // }
+
+    const button = event.target.innerHTML;
+    button === 'Pokaż' ? event.target.innerHTML = 'Ukryj' : event.target.innerHTML = 'Pokaż'; // eslint-disable-line
+  }
+
+  render() {
+    return (
+      <Layout path={this.path}>
+        <section className="main component-wrapper">
+          <div className="main__jumbo jumbo">
+            <div className="jumbo__title">{this.jumbotrone.title}</div>
+            <div className="jumbo__desc">{this.jumbotrone.description}</div>
+          </div>
+          <div className="main__body component_body">
+            <section className="main-desc">
+              <div className="main-desc__general">{this.description}</div>
+              <button type="button" className="btn main-desc__purposes--toggle" onClick={this.togglePurposes}>Pokaż</button>
+              <div ref={this.purposes} className="main-desc__purposes cms_content" dangerouslySetInnerHTML={{ __html: this.html }} />
+            </section>
+          </div>
         </section>
-      </div>
-    </section>
-  </Layout>
-);
+      </Layout>
+    );
+  }
+}
 
 const MainPage = ({ data }) => {
   const { markdownRemark: post } = data;
@@ -40,6 +67,7 @@ const MainPage = ({ data }) => {
 };
 
 export default MainPage;
+// export MainPageTemplate;
 
 MainPageTemplate.propTypes = {
   jumbotrone: PropTypes.object.isRequired,
