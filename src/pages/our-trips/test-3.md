96e2da2060ae9d0035f1683ac8fe27113a93dc95
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-10-19 09:15:02 +0200

    sortowanie i dropdown w cms

---
+++ src/pages/our-trips/test-3.md
@@ -0,0 +1,29 @@
+---
+templateKey: new-trip
+title: Test3
+date: 2017-10-22T10:46:11.548Z
+place: Kraków
+description: Dwudniowe zdobywanie Kopca Kościuszki
+tags:
+  - newTrip
+---
+# Główny opis wyjazdu
+
+Dynamically enhance mission-critical architectures without multimedia based outsourcing. Synergistically syndicate user-centric outsourcing rather than performance based total linkage. Authoritatively architect end-to-end e-services for best-of-breed deliverables. Completely parallel task worldwide innovation through market-driven opportunities. Authoritatively procrastinate magnetic testing procedures vis-a-vis market positioning strategic theme areas.
+
+## 
+Plan wyjazdu
+
+* wyjazd
+* item 1
+* item 2
+* item 3
+
+## Koszty
+
+Conveniently envisioneer just in time sources through principle-centered technologies. Efficiently incentivize B2B scenarios after adaptive collaboration and idea-sharing. Interactively conceptualize multifunctional content without bricks-and-clicks convergence. Conveniently utilize end-to-end mindshare after revolutionary markets. Uniquely reintermediate prospective best practices vis-a-vis vertical partnerships.
+
+## 
+Coś Tam
+
+Conveniently envisioneer just in time sources through principle-centered technologies. Efficiently incentivize B2B scenarios after adaptive collaboration and idea-sharing. Interactively conceptualize multifunctional content without bricks-and-clicks convergence. Conveniently utilize end-to-end mindshare after revolutionary markets. Uniquely reintermediate prospective best practices vis-a-vis vertical partnerships.
--- src/templates/trips.js
+++ src/templates/trips.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import nanoid from 'nanoid';
+import moment from 'moment';
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 
+const _ = require('lodash');
+
 const noNewTrips = {
-  node: {
-    frontmatter: {
-      title: 'Brak nowych wyjazdów',
-      date: ''
-    }
-  }
+  title: 'Brak wyjazdów'
 };
 
 const renderTrips = tripsArray => tripsArray.map(trip => (
-    <>
-      <div>{trip.node.frontmatter.title}</div>
-      <div>{trip.node.frontmatter.date}</div>
-      <div>{trip.node.frontmatter.place}</div>
-      <div><Link to={trip.node.fields.slug}>Zobacz więcej</Link></div>
-    </>
+  <div className="single-trip" key={nanoid()}>
+    <div>{trip.title}</div>
+    <div>{moment(trip.date).format('DD-MM-YYYY')}</div>
+    <div>{trip.place}</div>
+    <div><Link to={trip.slug}>Zobacz więcej</Link></div>
+  </div>
 ));
 
 export const TripsPageTemplate = ({ path, newTrips }) => (
@@ -26,10 +25,10 @@ export const TripsPageTemplate = ({ path, newTrips }) => (
     <section className="blogEntry component-wrapper">
       <div className="trips__jumbo jumbo">
         <div className="jumbo__title">Najbliższy wyjazd</div>
-        <div className="jumbo__title">{newTrips[0].node.frontmatter.title}</div>
-        <div className="jumbo__desc">{newTrips[0].node.frontmatter.date}</div>
-        <div className="jumbo__desc">{newTrips[0].node.frontmatter.place}</div>
-        <div className="jumbo__desc"><Link to={newTrips[0].node.fields.slug}>Zobacz więcej</Link></div>
+        <div className="jumbo__title">{newTrips[0].title}</div>
+        <div className="jumbo__desc">{moment(newTrips[0].date).format('DD-MM-YYYY')}</div>
+        <div className="jumbo__desc">{newTrips[0].place}</div>
+        <div className="jumbo__desc"><Link to={newTrips[0].slug}>Zobacz więcej</Link></div>
       </div>
       <div className="blogEntry_body component_body">
         <div className="blogEntry__first-line">Pozostałe wyjazdy</div>
@@ -43,10 +42,20 @@ const TripsPage = ({ data }) => {
   const { markdownRemark: post } = data;
   const { allMarkdownRemark: newTrips } = data;
 
+  const tripsData = newTrips.edges ? newTrips.edges.map((trip) => {
+    const tripData = {
+      title: trip.node.frontmatter.title,
+      date: trip.node.frontmatter.date,
+      place: trip.node.frontmatter.place,
+      slug: trip.node.fields.slug
+    };
+    return tripData;
+  }) : noNewTrips;
+
   return (
     <TripsPageTemplate
       path={post.frontmatter.path}
-      newTrips={newTrips ? newTrips.edges : [noNewTrips]}
+      newTrips={_.sortBy(tripsData, ['date'])}
     />
   );
 };
@@ -80,7 +89,7 @@ query TripPage($id: String!) {
         }
         frontmatter{
           title
-          date(formatString: "DD.MM.YYYY")
+          date
           place
         }
       }
