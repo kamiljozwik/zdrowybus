7e8f499b9bc6674604d5b4b21d4c8da61260da1b
kamil.jozwik <kamil.jozwik@erecruiter.pl>
2018-10-25 17:04:32 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,59 @@
 import React from 'react';
-// import Link from 'gatsby-link';
+import { StaticQuery, graphql } from 'gatsby';
+import Logo from '../img/Logo_small.png';
+import fb from '../img/facebook.svg';
+import yt from '../img/youtube.svg';
 
 const Footer = () => (
-    <footer className="footer">
-        <span>zdrowybus.pl - 2018 - All rights reserved</span>
-    </footer>
+  <StaticQuery
+    query={graphql`
+    query Footer {
+      allMarkdownRemark(
+      filter: {
+        frontmatter: {
+          templateKey: {eq: "contact"}
+        }
+      }
+    ) {
+        edges {
+          node {
+            id
+            frontmatter {
+              emailBus
+              emailArek
+            }
+          }
+        }
+      }
+    }
+      `}
+    render={dataQuery => (
+      <footer className="footer">
+        <div className="footer__logo">
+          <img
+            className="footer__logo__img"
+            src={Logo}
+            alt="sidebar logo"
+            role="presentation"
+          />
+        </div>
+        {/* <div className="footer__data">
+          <div className="footer__data--mail">
+            <span>{dataQuery.allMarkdownRemark.edges[0].node.frontmatter.emailBus}</span>
+            <span>{dataQuery.allMarkdownRemark.edges[0].node.frontmatter.emailArek}</span>
+          </div>
+          <div className="footer__data--social">
+            <a href="https://www.youtube.com" className="footer-yt">
+              <img src={yt} alt="YT" />
+            </a>
+            <a href="https://www.youtube.com" className="footer-fb">
+              <img src={fb} alt="FB" />
+            </a>
+          </div>
+        </div> */}
+      </footer>
+    )}
+  />
 );
 
 export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { CSSPlugin, AttrPlugin } from 'gsap/all';
 import Sidebar from './Sidebar';
+import Footer from './Footer';
 import '../styles/styles.scss';
 
 const plugins = [CSSPlugin, AttrPlugin]; // eslint-disable-line
 
 // const TemplateWrapper = ({ children }) => (
 const TemplateWrapper = props => (
-  <React.Fragment>
-    <div>
-      <Helmet
-        title="ZdrowyBus"
-        meta={[
-          { name: 'description', content: 'Sample' },
-          { name: 'keywords', content: 'sample, something' },
-        ]}
-      />
-      <Sidebar path={props.path} />
-      <div>{props.children}</div>
-    </div>
-  </React.Fragment>
+  <div id="container">
+    <Helmet
+      title="ZdrowyBus"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Sidebar path={props.path} />
+    <div>{props.children}</div>
+    <Footer />
+  </div>
 );
 
 TemplateWrapper.propTypes = {
